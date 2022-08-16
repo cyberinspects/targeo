1d86fb0b5cbd8921f7fcbbf0463020e75c60ef8e
Cyber Inspects <108610011+cyberinspects@users.noreply.github.com>
2022-08-16 16:14:11 +0500

    Update targeo.py

--- targeo.py
+++ targeo.py
@@ -4,7 +4,12 @@ import urllib3
 urllib3.disable_warnings()
 import argparse
 from colorama import Fore
-parser = argparse.ArgumentParser(description='Target range calculator and ip geo lookup tool.')
+from argparse import RawTextHelpFormatter
+
+
+
+parser = argparse.ArgumentParser(description=Fore.YELLOW+'Target range calculator and ip geo lookup tool.', formatter_class=RawTextHelpFormatter)
+
 parser.add_argument('-t','--type', type=str,
                     help='Range type (Allowed types are continent,nation and region)')
 
@@ -38,10 +43,9 @@ print(Fore.RED+"""
 ⡏⠀ ⠀⠀⠀⠀   ⠀Target Any Thing
 ⠧ [@cyberinspects] [ @kaleemibnawar]
     """)
-print(Fore.YELLOW+"Results are being loaded:")
 
 args = parser.parse_args()
-cookies = {}
+request=''
 types=str(args.type)
 types.lower()
 ip=str(args.ip)
@@ -57,26 +61,27 @@ if args.type:
     elif types in ('region','r','re','reg','regi','regio'):
         request=f'https://cyberinspects-targeo.herokuapp.com/region/{inputs}'
     else:
-        print("Unknown range type was chosen!")
+        print("Unknown target type was chosen!")
         exit()
-
 elif args.ip:
     request=f'https://cyberinspects-targeo.herokuapp.com/geoip/{args.ip}'
 else:
-    print('none')
+    print(Fore.RED+'Invalid or no target selected to be ranged. [use -h parameter to learn about targeo]')
 
-
-response = requests.post(request,verify=False)
-page=response.content
-page_soup = soup(page, "html.parser") 
-content=page_soup
-
-if args.output is not None:
-    with open(output,'a+') as file:
-        file.write(content)
-print(Fore.GREEN+str(content))
-
-
+if request!='':
+    response = requests.post(request,verify=False)
+    page=response.content
+    page_soup = soup(page, "html.parser") 
+    content=page_soup
+    if args.output is not None:
+        with open(output,'a+') as file:
+            file.write(content)
+    if str(content).strip()!="":
+        print(Fore.GREEN+str(content))
+    else:
+        print('No data returned, Possibly you have asked a quey unknown to our system.')
+else:
+    pass
 
 
 
